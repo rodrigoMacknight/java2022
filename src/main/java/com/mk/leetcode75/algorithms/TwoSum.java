db84c3eabf5ae98f2bdcf45819e18d32e078dfde
RodrigoMacknight <rodrigo.macknight@gmail.com>
2022-06-20 21:14:50 -0300

    feature/twosum

--- src/main/java/com/mk/leetcode75/algorithms/TwoSum.java
+++ src/main/java/com/mk/leetcode75/algorithms/TwoSum.java
@@ -18,18 +18,13 @@ Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 public class TwoSum {
 
     public int[] twoSum(int[] nums, int target) {
-        int[] ret = null;
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > target) {
-                continue;
-            }
-            for (int j = i; j < nums.length; j++) {
+            for (int j = i+1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
-                    ret = new int[]{i, j};
-                    break;
+                    return new int[]{i, j};
                 }
             }
         }
-        return ret;
+        return null;
     }
 }
--- src/test/java/com/mk/leetcode75/algorithms/TwoSumTest.java
+++ src/test/java/com/mk/leetcode75/algorithms/TwoSumTest.java
@@ -7,11 +7,32 @@ import static org.junit.jupiter.api.Assertions.*;
 public class TwoSumTest {
     TwoSum toTest;
 
+
+
     @Test
     void isTwoSumCorrect() {
         toTest = new TwoSum();
         int[] nums = {2,7,11,15};
         int[] ret = {0,1};
-        assertArrayEquals(toTest.twoSum(nums, 9), ret);
+        assertArrayEquals(ret, toTest.twoSum(nums, 9));
     }
+
+    @Test
+    void anotherArrayTest() {
+        toTest = new TwoSum();
+        int[] nums = {3,3};
+        int[] ret = {0,1};
+        assertArrayEquals(ret, toTest.twoSum(nums, 6));
+    }
+    @Test
+    void yetAnotherArrayTest() {
+        toTest = new TwoSum();
+
+        int[] nums = {-1,-2,-3,-4,-5};
+        int[] ret = {2,4};
+        assertArrayEquals(ret, toTest.twoSum(nums,   -8));
+    }
+
+
+
 }
